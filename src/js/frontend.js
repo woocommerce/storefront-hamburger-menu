2b7c49336d4604109ef4f93c43723415be458a2b
Niels Lange <info@nielslange.de>
2021-07-22 12:03:42 +0200

    Add build tools

---
+++ src/js/frontend.js
@@ -0,0 +1,20 @@
+( function( $ ) {
+	$( function() {
+		$( '.main-navigation .handheld-navigation, .main-navigation div.menu' ).prepend( '<span class="shm-close">' + shm_i18n.close + '</span>' ); /* jshint ignore:line */
+		$( '.shm-close' ).on( 'click', function() {
+			$( '.menu-toggle' ).trigger( 'click' );
+		} );
+
+		$( document ).click( function( event ) {
+			var menuContainer = $( '.main-navigation' );
+
+			if ( $( '.main-navigation' ).hasClass( 'toggled' ) ) {
+				if ( ! menuContainer.is( event.target ) && 0 === menuContainer.has( event.target ).length ) {
+					event.preventDefault();
+					event.stopPropagation();
+					$( '.menu-toggle' ).trigger( 'click' );
+				}
+			}
+		} );
+	} );
+} )( jQuery );
